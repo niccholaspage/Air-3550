3f104c715cbcfca494a336f4e2c34e1ed70cbef6
niccholaspage <niccholaspage@gmail.com>
2021-03-30 00:01:28 -0400

    Add DB context for EF Core

---
+++ Air 3550/Repository/AirContext.cs
@@ -0,0 +1,20 @@
+﻿using Air_3550.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Air_3550.Repository
+{
+    class AirContext : DbContext
+    {
+        public DbSet<Airport> Airports { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<CustomerData> CustomerDatas { get; set; }
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<FlightScheduleEvent> FlightScheduleEvents { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<ScheduledFlight> ScheduledFlights { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite(@"Data Source=blogging.db");
+    }
+}
