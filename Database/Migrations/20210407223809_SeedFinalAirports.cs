138b5a9e7c55e824bf26935dbe92ea50df2efaec
niccholaspage <niccholaspage@gmail.com>
2021-04-07 18:38:23 -0400

    Add 5 remaining airports

---
+++ Database/Migrations/20210407223809_SeedFinalAirports.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class SeedFinalAirports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "AirportId", "City", "Code", "Elevation", "Latitude", "Longitude", "State" },
+                values: new object[] { 6, "Los Angeles", "LAX", 128, 33.9425m, -118.408056m, "California" });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "AirportId", "City", "Code", "Elevation", "Latitude", "Longitude", "State" },
+                values: new object[] { 7, "Chicago", "ORD", 668, 41.978611m, -87.904722m, "Illinois" });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "AirportId", "City", "Code", "Elevation", "Latitude", "Longitude", "State" },
+                values: new object[] { 8, "Dallas", "DFW", 607, 32.896944m, -97.038056m, "Ohio" });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "AirportId", "City", "Code", "Elevation", "Latitude", "Longitude", "State" },
+                values: new object[] { 9, "Denver", "DEN", 5434, 39.861667m, -104.673056m, "Colorado" });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "AirportId", "City", "Code", "Elevation", "Latitude", "Longitude", "State" },
+                values: new object[] { 10, "Seattle", "SEA", 433, 47.448889m, -122.309444m, "Washington" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 10);
+        }
+    }
+}
--- Database/Repository/AirContext.cs
+++ Database/Repository/AirContext.cs
@@ -26,7 +26,12 @@ namespace Air_3550.Repository
                 new Airport { AirportId = 2, Code = "BNA", City = "Nashville", State = "Tennessee", Latitude = 36.126667m, Longitude = -86.681944m, Elevation = 599 },
                 new Airport { AirportId = 3, Code = "DTW", City = "Detroit", State = "Michigan", Latitude = 42.2125m, Longitude = -83.353333m, Elevation = 645 },
                 new Airport { AirportId = 4, Code = "ATL", City = "Atlanta", State = "Georgia", Latitude = 33.636667m, Longitude = -84.428056m, Elevation = 1026 },
-                new Airport { AirportId = 5, Code = "JFK", City = "New York City", State = "New York", Latitude = 40.639722m, Longitude = -73.778889m, Elevation = 13 }
+                new Airport { AirportId = 5, Code = "JFK", City = "New York City", State = "New York", Latitude = 40.639722m, Longitude = -73.778889m, Elevation = 13 },
+                new Airport { AirportId = 6, Code = "LAX", City = "Los Angeles", State = "California", Latitude = 33.9425m, Longitude = -118.408056m, Elevation = 128 },
+                new Airport { AirportId = 7, Code = "ORD", City = "Chicago", State = "Illinois", Latitude = 41.978611m, Longitude = -87.904722m, Elevation = 668 },
+                new Airport { AirportId = 8, Code = "DFW", City = "Dallas", State = "Ohio", Latitude = 32.896944m, Longitude = -97.038056m, Elevation = 607 },
+                new Airport { AirportId = 9, Code = "DEN", City = "Denver", State = "Colorado", Latitude = 39.861667m, Longitude = -104.673056m, Elevation = 5434 },
+                new Airport { AirportId = 10, Code = "SEA", City = "Seattle", State = "Washington", Latitude = 47.448889m, Longitude = -122.309444m, Elevation = 433 }
                 );
         }
 
