5ab307150a270167e591301aee2b8dcd138fed0f
niccholaspage <niccholaspage@gmail.com>
2021-04-07 19:00:17 -0400

    Implement Flight.GetDistance

--- Database/Models/Flight.cs
+++ Database/Models/Flight.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Air_3550.Models
 {
@@ -14,5 +15,29 @@ namespace Air_3550.Models
 
         [Required]
         public Airport DestinationAirport { get; set; }
+
+        public double GetDistance()
+        {
+            static double toRadians(decimal angdeg)
+            {
+                return (double)angdeg / 180.0 * Math.PI;
+            }
+
+            const int R = 6371; // Radius of the earth
+
+            double latDistance = toRadians(DestinationAirport.Latitude - OriginAirport.Latitude);
+            double lonDistance = toRadians(DestinationAirport.Longitude - OriginAirport.Longitude);
+            double a = Math.Sin(latDistance / 2) * Math.Sin(latDistance / 2)
+                    + Math.Cos(toRadians(OriginAirport.Latitude)) * Math.Cos(toRadians(DestinationAirport.Latitude))
+                    * Math.Sin(lonDistance / 2) * Math.Sin(lonDistance / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double distance = R * c * 1000; // convert to meters
+
+            double height = OriginAirport.Elevation - DestinationAirport.Elevation;
+
+            distance = Math.Pow(distance, 2) + Math.Pow(height, 2);
+
+            return Math.Sqrt(distance);
+        }
     }
 }
