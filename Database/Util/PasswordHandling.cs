c24b7920c01b28c6e213cd2f35ac7f3961c6c02a
niccholaspage <niccholaspage@gmail.com>
2021-04-08 09:26:53 -0400

    Add PasswordHandling.CheckPassword function

--- Database/Util/PasswordHandling.cs
+++ Database/Util/PasswordHandling.cs
@@ -8,12 +8,17 @@ namespace Database.Util
     {
         public static string HashPassword(string password)
         {
-            // TODO: Ask Larry if this seems reasonable because this is ACTUALLY stupid.
+            // TODO: Ask Larry if this seems reasonable because this is ACTUALLY so stupid and insecure.
             byte[] data = Encoding.UTF8.GetBytes(password);
             byte[] result;
             SHA512 shaM = new SHA512Managed();
             result = shaM.ComputeHash(data);
             return Convert.ToBase64String(result);
+        }
+
+        public static bool CheckPassword(string password, string hash)
+        {
+            return HashPassword(password) == hash;
         }
     }
 }
