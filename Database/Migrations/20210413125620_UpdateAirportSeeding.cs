b7b4f7050d5c4846ab88025e11779898eb72e9e6
niccholaspage <niccholaspage@gmail.com>
2021-04-13 08:57:24 -0400

    Switch to LGA and MDW from JFK and ORD because they have more domestic flights

---
+++ Database/Migrations/20210413125620_UpdateAirportSeeding.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class UpdateAirportSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 5,
+                columns: new[] { "Code", "Elevation", "Latitude", "Longitude" },
+                values: new object[] { "LGA", 21, 40.775m, -73.875m });
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 7,
+                columns: new[] { "Code", "Latitude", "Longitude" },
+                values: new object[] { "MDW", 41.786111m, -87.7525m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 5,
+                columns: new[] { "Code", "Elevation", "Latitude", "Longitude" },
+                values: new object[] { "JFK", 13, 40.639722m, -73.778889m });
+
+            migrationBuilder.UpdateData(
+                table: "Airports",
+                keyColumn: "AirportId",
+                keyValue: 7,
+                columns: new[] { "Code", "Latitude", "Longitude" },
+                values: new object[] { "ORD", 41.978611m, -87.904722m });
+        }
+    }
+}
