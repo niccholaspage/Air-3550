849f74d182e1e3b0cb7cb95d1ee4efe0fdc66c50
niccholaspage <niccholaspage@gmail.com>
2021-04-14 13:04:02 -0400

    Create a LoginViewModel for use with LoginPage

---
+++ Air 3550/Air 3550/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Air_3550.Repository;
+using Database.Util;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Air_3550.ViewModels
+{
+    class LoginViewModel : ObservableObject
+    {
+        private string _username;
+
+        public string Username
+        {
+            get => _username;
+            set => SetProperty(ref _username, value);
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        private string _feedback;
+
+        public string Feedback
+        {
+            get => _feedback;
+            set => SetProperty(ref _feedback, value);
+        }
+
+        public async Task<bool> PerformLogin()
+        {
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                Feedback = "Please enter a username or password.";
+
+                return false;
+            }
+
+            using (var db = new AirContext())
+            {
+                var user = await db.Users.SingleOrDefaultAsync(user => user.LoginId == Username);
+
+                if (user == null)
+                {
+                    Feedback = "The username or password is incorrect.";
+
+                    return false;
+                }
+                else
+                {
+                    if (PasswordHandling.CheckPassword(Password, user.PasswordHash))
+                    {
+                        Feedback = null;
+
+                        return true;
+                    }
+                    else
+                    {
+                        Feedback = "The username or password is incorrect.";
+
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
--- Air 3550/Air 3550/Views/LoginPage.xaml.cs
+++ Air 3550/Air 3550/Views/LoginPage.xaml.cs
@@ -2,6 +2,7 @@
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 //test jh
 using Air_3550.Repository;
+using Air_3550.ViewModels;
 using Database.Util;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.UI.Xaml;
@@ -21,44 +22,7 @@ namespace Air_3550.Views
             this.InitializeComponent();
         }
 
-        private async Task PerformLogin()
-        {
-            void ShowFeedback(string text)
-            {
-                Feedback.Text = text;
-                Feedback.Visibility = Visibility.Visible;
-            }
-
-            string username = UsernameBox.Text;
-            string password = PasswordBox.Password;
-
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
-            {
-                ShowFeedback("Please enter a username or password.");
-            }
-
-            using (var db = new AirContext())
-            {
-                var user = await db.Users.SingleOrDefaultAsync(user => user.LoginId == username);
-
-                if (user == null)
-                {
-                    ShowFeedback("The username or password is incorrect.");
-                }
-                else
-                {
-                    if (PasswordHandling.CheckPassword(password, user.PasswordHash))
-                    {
-                        // TODO: Actually sign you in.
-                        Frame.Navigate(typeof(MainPage));
-                    }
-                    else
-                    {
-                        ShowFeedback("The username or password is incorrect.");
-                    }
-                }
-            }
-        }
+        LoginViewModel ViewModel = new LoginViewModel();
 
         public async void LoginButton_Clicked(object sender, RoutedEventArgs e)
         {
@@ -70,6 +34,14 @@ namespace Air_3550.Views
             if (e.Key == VirtualKey.Enter)
             {
                 await PerformLogin();
+            }
+        }
+
+        private async Task PerformLogin()
+        {
+            if (await ViewModel.PerformLogin())
+            {
+                Frame.Navigate(typeof(MainPage));
             }
         }
 
