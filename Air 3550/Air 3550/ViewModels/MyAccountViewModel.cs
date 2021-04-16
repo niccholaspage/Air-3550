c1dcbeac67800c9715c3a7cd0b2d03099114ebab
niccholaspage <niccholaspage@gmail.com>
2021-04-15 20:51:00 -0400

    Work on tab selection in MyAccountPage

---
+++ Air 3550/Air 3550/ViewModels/MyAccountViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+
+namespace Air_3550.ViewModels
+{
+    class MyAccountViewModel : ObservableValidator
+    {
+        private bool _viewingBookings = true;
+
+        public bool ViewingBookings
+        {
+            get => _viewingBookings;
+            set
+            {
+                SetProperty(ref _viewingBookings, value);
+
+                OnPropertyChanged(nameof(ViewingAccountInfo));
+            }
+        }
+
+        public bool ViewingAccountInfo
+        {
+            get => !ViewingBookings;
+        }
+    }
+}
--- Air 3550/Air 3550/Views/MyAccountPage.xaml.cs
+++ Air 3550/Air 3550/Views/MyAccountPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Air_3550.ViewModels;
+using Microsoft.UI.Xaml.Controls;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -13,6 +14,18 @@ namespace Air_3550.Views
         public MyAccountPage()
         {
             this.InitializeComponent();
+        }
+
+        MyAccountViewModel ViewModel = new();
+
+        private void BookingsButton_Click(object _, Microsoft.UI.Xaml.RoutedEventArgs _1)
+        {
+            ViewModel.ViewingBookings = true;
+        }
+
+        private void AccountInfoButton_Click(object _, Microsoft.UI.Xaml.RoutedEventArgs _1)
+        {
+            ViewModel.ViewingBookings = false;
         }
     }
 }
