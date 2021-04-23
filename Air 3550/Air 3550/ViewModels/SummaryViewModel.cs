7e4e2f45792c252f7f2029aceb33894025b66086
hammittej <jacob.hammitte@gmail.com>
2021-04-23 13:30:11 -0400

    Added crude summary to .csv

--- Air 3550/Air 3550/ViewModels/SummaryViewModel.cs
+++ Air 3550/Air 3550/ViewModels/SummaryViewModel.cs
@@ -9,12 +9,14 @@ using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.Provider;
 using FileSavePicker = Windows.Storage.Pickers.FileSavePicker;
+using System.IO;
 
 namespace Air_3550.Views
 {
     class SummaryViewModel : ObservableValidator
     {
         private List<ScheduledFlight> _sflights;
+        private List<string> lines = new List<string>();
 
         public List<ScheduledFlight> Sflights
         {
@@ -29,6 +31,8 @@ namespace Air_3550.Views
                 Sflights = await db.ScheduledFlights
                     .Include(ScheduledFlight => ScheduledFlight.Flight.DestinationAirport)
                     .Include(ScheduledFlight => ScheduledFlight.Flight.OriginAirport)
+                    .Include(ScheduledFlight => ScheduledFlight.Flight.Plane)
+                    .Include(ScheduledFlight => ScheduledFlight.Tickets)
                     .ToListAsync();
             }
         }
@@ -43,35 +47,26 @@ namespace Air_3550.Views
             MainWindow.FixPicker(savePicker);
 
             // Dropdown of file types the user can save the file as
-            savePicker.FileTypeChoices.Add("Rich Text", new List<string>() { ".rtf" });
+            savePicker.FileTypeChoices.Add(".csv", new List<string>() { ".csv" });
 
             // Default file name if the user does not type one in or select a file to replace
             savePicker.SuggestedFileName = "New Document";
 
             StorageFile file = await savePicker.PickSaveFileAsync();
 
-            if (file != null)
-            {
-                // Prevent updates to the remote version of the file until we 
-                // finish making changes and call CompleteUpdatesAsync.
-                CachedFileManager.DeferUpdates(file);
-                // write to file
-                using (Windows.Storage.Streams.IRandomAccessStream randAccStream =
-                    await file.OpenAsync(Windows.Storage.FileAccessMode.ReadWrite))
-                {
-                    //editor.Document.SaveToStream(Windows.UI.Text.TextGetOptions.FormatRtf, randAccStream);
-                }
+            lines.Add("ScheduledFlightId,Capacity,Tickets,Cost Per Ticket");
 
-                // Let Windows know that we're finished changing the file so the 
-                // other app can update the remote version of the file.
-                FileUpdateStatus status = await CachedFileManager.CompleteUpdatesAsync(file);
-                if (status != FileUpdateStatus.Complete)
-                {
-                    Windows.UI.Popups.MessageDialog errorBox =
-                        new Windows.UI.Popups.MessageDialog("File " + file.Name + " couldn't be saved.");
-                    await errorBox.ShowAsync();
-                }
+            foreach (ScheduledFlight sF in Sflights)
+            {
+                lines.Add(
+                    sF.ScheduledFlightId + "," + sF.Flight.Plane.MaxSeats + "," + sF.Tickets.Count + "," + sF.Flight.GetCost()
+                    );
             }
+
+            //Grabs File Path to write using System.IO
+            String FilePath = file.Path;
+            File.WriteAllLines(FilePath, lines);
+            
         }
     }
 }
