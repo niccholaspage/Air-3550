15b04d4d906128c26024f2bb78aeeb67cfebda8a
hammittej <jacob.hammitte@gmail.com>
2021-04-25 14:42:04 -0400

    Query Complete for booking objects

--- Air 3550/Air 3550/ViewModels/BookingsViewModel.cs
+++ Air 3550/Air 3550/ViewModels/BookingsViewModel.cs
@@ -8,12 +8,14 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using Air_3550.Models;
+using Air_3550.Util;
+using System.Collections.Generic;
 
 namespace Air_3550.ViewModels
 {
     class BookingsViewModel
     {
-        public ObservableCollection<Booking> BookingsC = new();
+        public ObservableCollection<FlightPathWithDate> BookingsC = new();
         public ObservableCollection<Ticket> TicketsC = new();
 
         private readonly UserSessionService _userSessionService;
@@ -25,9 +27,9 @@ namespace Air_3550.ViewModels
 
         public async void GetBookings()
         {
+            //Grab all useful data related to a booking
             using (var db = new AirContext())
             {
-                // TODO: How do we make this async?
                 var bookings = await db.Bookings
                     .Where(Booking => Booking.UserId == _userSessionService.UserId)
                     .Include(Booking => Booking.Tickets)
@@ -39,11 +41,16 @@ namespace Air_3550.ViewModels
                     .ThenInclude(ScheduledFlight => ScheduledFlight.Flight)
                     .ThenInclude(Flight => Flight.DestinationAirport)
                     .ToListAsync();
+                //Turns booking items into FlightPaths 
+                List<Flight> adding = new List<Flight>();
                 foreach (Booking a in bookings)
                 {
-                    BookingsC.Add(a);
+                    foreach(Ticket b in a.Tickets)
+                    {
+                        adding.Add(b.ScheduledFlight.Flight);
+                    }
+                    BookingsC.Add(new FlightPathWithDate(new FlightPath(adding.ToArray()), a.Tickets.First().ScheduledFlight.DepartureDate));                
                 }
-
             }
         }
     }
