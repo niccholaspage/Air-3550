f39c372d938ec53e4e0c0041917ba830a838a70d
niccholaspage <niccholaspage@gmail.com>
2021-04-24 22:02:11 -0400

    Stop login button from showing when you are already on the login/register page

--- Air 3550/Air 3550/Controls/HeaderControl.xaml.cs
+++ Air 3550/Air 3550/Controls/HeaderControl.xaml.cs
@@ -3,13 +3,15 @@ using Air_3550.Views;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System.ComponentModel;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Air_3550.Controls
 {
-    public sealed partial class HeaderControl : UserControl
+    public sealed partial class HeaderControl : UserControl, INotifyPropertyChanged
     {
         private readonly UserSessionService userSession;
 
@@ -19,13 +21,50 @@ namespace Air_3550.Controls
             set => SetValue(ContentFrameProperty, value);
         }
 
+        public bool ShowLoginButton
+        {
+            get
+            {
+                return !userSession.IsLoggedIn
+                && ContentFrame.CurrentSourcePageType != typeof(LoginPage)
+                && ContentFrame.CurrentSourcePageType != typeof(RegisterPage);
+            }
+        }
+
         public static readonly DependencyProperty ContentFrameProperty = DependencyProperty.Register(nameof(ContentFrame), typeof(Frame), typeof(AirportSuggestBox), new PropertyMetadata(default(Frame)));
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public HeaderControl()
         {
             userSession = App.Current.Services.GetService<UserSessionService>();
 
             this.InitializeComponent();
+
+            this.Loaded += (_, __) =>
+            {
+                userSession.PropertyChanged += HandleUserSessionChange;
+                ContentFrame.Navigated += HandleFrameNavigation;
+            };
+
+            this.Unloaded += (_, __) =>
+            {
+                userSession.PropertyChanged -= HandleUserSessionChange;
+                ContentFrame.Navigated -= HandleFrameNavigation;
+            };
+        }
+
+        private void HandleUserSessionChange(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(UserSessionService.UserId))
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowLoginButton)));
+            };
+        }
+
+        private void HandleFrameNavigation(object sender, NavigationEventArgs e)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowLoginButton)));
         }
 
         private void LoginButton_Click(object _sender, RoutedEventArgs _1)
