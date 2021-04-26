aa4d675683a3a25096d3106d54dbdb32bb605693
niccholaspage <niccholaspage@gmail.com>
2021-04-26 02:45:40 -0400

    Add new extension method for formatting decimals as money

---
+++ Database/Util/ExtensionMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace Database.Util
+{
+    public static class ExtensionMethods
+    {
+        public static string FormatAsMoney(this decimal amount)
+        {
+            return string.Format("{0:C}", amount);
+        }
+    }
+}
