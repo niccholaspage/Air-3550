d27bf7c08317281bf7c39abdb971d58f9c46cf6b
niccholaspage <niccholaspage@gmail.com>
2021-04-27 16:14:28 -0400

    Add comments to ScheduledFlight

--- Database/Models/ScheduledFlight.cs
+++ Database/Models/ScheduledFlight.cs
@@ -10,6 +10,16 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * The scheduled flight model is used to represent a
+ * scheduled flight in the database. In our system, a
+ * scheduled flight refers to a specific instance of a
+ * flight occurring on a specific date. It does NOT
+ * include any information that the flight model includes,
+ * so it does have a refer to the flight for all of
+ * that information.
+ */
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -22,24 +32,37 @@ namespace Air_3550.Models
     {
         public int ScheduledFlightId { get; set; } // The primary ID of the scheduled flight, used to relate the scheduled flight to other data.
 
-        public int FlightId { get; set; }
+        public int FlightId { get; set; } // The ID of the flight that this scheduled flight is based from.
 
         [Required]
-        public Flight Flight { get; set; }
+        public Flight Flight { get; set; } // The flight that this scheduled flight is based from.
 
+        // The departure date of this scheduled flight - this is
+        // exclusively a date, so the time will always be midnight.
         [Required]
         public DateTime DepartureDate { get; set; }
 
+        // A computed property formatting the departure date of the scheduled
+        // flight nicely for UI bindings.
         [NotMapped]
         public string FormattedDepartureDate => DepartureDate.FormatNicely();
 
+        // A list of all the tickets that are attached to
+        // this scheduled flight, which can be used for determining
+        // how full this scheduled flight is.
         public List<Ticket> Tickets { get; }
 
+        // A convenience method to return the exact timestamp
+        // a scheduled flight departs by taking the departure date
+        // and adding the base flight's departure time to it.
         public DateTime GetDepartureTimestamp()
         {
             return DepartureDate.Add(Flight.DepartureTime);
         }
 
+        // A convenience method to return the exact timestamp
+        // a scheduled flight arrives at by taking the departure
+        // timestamp and adding the base flight's duration to it.
         public DateTime GetArrivalTimestamp()
         {
             return GetDepartureTimestamp().Add(Flight.GetDuration());
