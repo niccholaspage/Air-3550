7218a1f8a7041e46ee8d909ca011ca91ea58487d
niccholaspage <niccholaspage@gmail.com>
2021-04-27 18:01:54 -0400

    Add comments to extension methods

--- Air 3550/Air 3550/Util/ExtensionMethods.cs
+++ Air 3550/Air 3550/Util/ExtensionMethods.cs
@@ -10,6 +10,15 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/*
+ * This class exists to add extension methods
+ * to various classes. The reason this file
+ * is contained in the Air 3550 project is due
+ * to it's use of the MVVM toolkit from
+ * Microsoft, which only the Air 3550 project
+ * depends on.
+ */
+
 using System.Linq;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 
@@ -17,18 +26,24 @@ namespace Air_3550.Util
 {
     public static class Extensions
     {
+        // A method that simply takes an ObservableValidator
+        // and returns the first error from it if one exists,
+        // otherwise returning an empty string. Unfortunately,
+        // WinUI does not support input validation on the
+        // UI elements, so we couldn't show the user per-field
+        // validation, so this is the next best thing - we simply
+        // get the first error and show them it.
         public static string GetFirstError(this ObservableValidator validator)
         {
+            // We get the errors from the validator, passing in null
+            // as the property name (this returns errors for every property)
+            // then we get the first one, or null if none exist.
             var firstError = validator.GetErrors(null).FirstOrDefault();
 
-            if (firstError != null)
-            {
-                return firstError.ErrorMessage;
-            }
-            else
-            {
-                return "";
-            }
+            // We then either return an empty string
+            // or the error message depending on if
+            // it exists.
+            return firstError != null ? firstError.ErrorMessage : "";
         }
     }
 }
--- Database/Util/ExtensionMethods.cs
+++ Database/Util/ExtensionMethods.cs
@@ -10,24 +10,41 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/*
+ * This class exists to add extension methods
+ * to various classes. The reason this file
+ * is contained in the Database project is due
+ * to it's use in some of the database models.
+ */
+
 using System;
 
 namespace Database.Util
 {
     public static class ExtensionMethods
     {
+        // First up, a simple method that takes a decimal
+        // and formats it as money. for example,
+        // 50.00 would be converted to $50.00.
         public static string FormatAsMoney(this decimal amount)
         {
             return string.Format("{0:C}", amount);
         }
 
+        // A method that simply formats a DateTime into
+        // a format as such: Sun, 04/19
         public static string FormatNicely(this DateTime dateTime) => dateTime.ToString("ddd, MM/dd");
 
+        // A method that simply formats a TimeSpan into
+        // a format like 4:09 PM.
         public static string FormatAsTimeNicely(this TimeSpan timeSpan)
         {
             return DateTime.Today.Add(timeSpan).ToString("h:mm tt");
         }
 
+        // A method that formats a TimeSpan as a duration
+        // as such: 1d 12h 3m. If a component is zero,
+        // it is not included in the string.
         public static string FormatAsDurationNicely(this TimeSpan timeSpan)
         {
             string result = "";
