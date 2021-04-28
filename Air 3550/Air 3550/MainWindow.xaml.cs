269f70b2e188bdc4f6307eea1a843a78aa378382
niccholaspage <niccholaspage@gmail.com>
2021-04-27 21:46:17 -0400

    Add comments to MainWindow

--- Air 3550/Air 3550/MainWindow.xaml.cs
+++ Air 3550/Air 3550/MainWindow.xaml.cs
@@ -10,15 +10,18 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * The main and only window of Air 3550.
+ * This window contains the main Frame.
+ * used to interact with the application
+ */
+
 using System;
 using System.Runtime.InteropServices;
 using Air_3550.Views;
 using Microsoft.UI.Xaml;
 using Windows.Storage.Pickers;
 using WinRT;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Air_3550
 {
@@ -27,17 +30,26 @@ namespace Air_3550
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        private static Window Instance;
+
         public MainWindow()
         {
             this.InitializeComponent();
 
-            Title = "Air 3550";
+            Title = "Air 3550"; // Set the window title.
 
-            ContentFrame.Navigate(typeof(MainPage));
+            ContentFrame.Navigate(typeof(MainPage)); // Navigate the user to the main page.
 
-            Instance = this;
+            Instance = this; // Set the singleton instance to this instance.
         }
 
+        // I am so, so sorry for this awful code Dr. Thomas. We experienced an issue with the
+        // FilePicker (an exception when trying to open a file picker dialog) found here:
+        // (https://github.com/microsoft/ProjectReunion/issues/466#issuecomment-779621766)
+        // To fix this issue, access to Win32 UIs are required, so we needed to implement
+        // this code to fix the issue.
+        //
+        // We start by defining interfaces to access the Win32 Window API.
         [ComImport]
         [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
@@ -53,15 +65,18 @@ namespace Air_3550
             IntPtr WindowHandle { get; }
         }
 
-        private static Window Instance;
-
+        // This method fixes a FileSavePicker by first grabbing
+        // the singleton MainWindow's WindowHandle, then call's
+        // the FileSavePicker's Initialize method (from the Win32
+        // API), passing in the WindowHandle, fixing the save
+        // picker.
         public static void FixPicker(FileSavePicker filePicker)
         {
-            //Get the Window's HWND
+            // Get the Window's HWND
             var hwnd = Instance.As<IWindowNative>().WindowHandle;
 
-            //Make folder Picker work in Win32
-
+            // Stop the folder picker from crashing
+            // when prompted to pick a file.
             var initializeWithWindow = filePicker.As<IInitializeWithWindow>();
             initializeWithWindow.Initialize(hwnd);
         }
