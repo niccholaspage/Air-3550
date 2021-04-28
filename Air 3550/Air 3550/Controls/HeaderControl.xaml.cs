b405e06f5676e49c92f66ec0ce9e6c1f357a296e
niccholaspage <niccholaspage@gmail.com>
2021-04-27 22:33:40 -0400

    Add comments to HeaderControl

--- Air 3550/Air 3550/Controls/HeaderControl.xaml.cs
+++ Air 3550/Air 3550/Controls/HeaderControl.xaml.cs
@@ -10,6 +10,12 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * This control is used for the header of the application,
+ * exposing back, login, and my account buttons based on the
+ * state of the application.
+ */
+
 using System.ComponentModel;
 using Air_3550.Services;
 using Air_3550.Views;
@@ -18,20 +24,13 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Air_3550.Controls
 {
     public sealed partial class HeaderControl : UserControl, INotifyPropertyChanged
     {
+        // We need the user's session to determine what
+        // buttons we need to show in the header control.
         private readonly UserSessionService userSession;
-
-        public Frame ContentFrame
-        {
-            get => (Frame)GetValue(ContentFrameProperty);
-            set => SetValue(ContentFrameProperty, value);
-        }
 
         public bool ShowLoginButton
         {
@@ -43,16 +42,31 @@ namespace Air_3550.Controls
             }
         }
 
+        // We expose a ContentFrame property to allow consumers
+        // to bind their own Frame so that the header control can
+        // navigate to different pages in the frame.
+        public Frame ContentFrame
+        {
+            get => (Frame)GetValue(ContentFrameProperty);
+            set => SetValue(ContentFrameProperty, value);
+        }
+
         public static readonly DependencyProperty ContentFrameProperty = DependencyProperty.Register(nameof(ContentFrame), typeof(Frame), typeof(HeaderControl), new PropertyMetadata(default(Frame)));
 
+        // We setup a PropertyChanged event to properly notify
+        // the UI whether to display the login button when the
+        // user is already on the login/registration page.
         public event PropertyChangedEventHandler PropertyChanged;
 
         public HeaderControl()
         {
+            // We grab the user session service from our services provider.
             userSession = App.Current.Services.GetService<UserSessionService>();
 
             this.InitializeComponent();
 
+            // We register and unload our two events we would like to
+            // handle in this.Loaded and this.Unloaded.
             this.Loaded += (_, __) =>
             {
                 userSession.PropertyChanged += HandleUserSessionChange;
@@ -66,6 +80,11 @@ namespace Air_3550.Controls
             };
         }
 
+        // In this method, we handle when a user logs in or out
+        // so that we can update the display of the login button.
+        // We then call the PropertyChanged event passing the name
+        // of the ShowLoginButton variable so that our UI
+        // updates the visibility of the button.
         private void HandleUserSessionChange(object sender, PropertyChangedEventArgs e)
         {
             if (e.PropertyName == nameof(UserSessionService.UserId))
@@ -74,21 +93,35 @@ namespace Air_3550.Controls
             };
         }
 
+        // In this method, we handle when a frame navigation event
+        // occurs so that we can update the display of the login
+        // button. We then call the PropertyChanged event passing
+        // the name of the ShowLoginButton variable so that our UI
+        // updates the visibility of the button.
         private void HandleFrameNavigation(object sender, NavigationEventArgs e)
         {
             PropertyChanged(this, new PropertyChangedEventArgs(nameof(ShowLoginButton)));
         }
 
+        // This method handles the login button click by simply
+        // navigating the frame to the login page.
         private void LoginButton_Click(object _sender, RoutedEventArgs _1)
         {
             ContentFrame.Navigate(typeof(LoginPage));
         }
 
+        // This method handles the my account button click by simply
+        // navigating the frame to the my account page.
         private void AccountInfoButton_Click(object _, RoutedEventArgs _1)
         {
             ContentFrame.Navigate(typeof(MyAccountPage));
         }
 
+
+        // This method handles the logout button click by logging the
+        // user out with the user session, navigating them to the main
+        // page, then clearing their entire backstack so they cannot
+        // navigate back to authenticated parts of the system.
         private void LogoutButton_Click(object _, RoutedEventArgs _1)
         {
             userSession.Logout();
@@ -98,9 +131,11 @@ namespace Air_3550.Controls
             ContentFrame.BackStack.Clear();
         }
 
+        // This method handles the back button click by simply
+        // navigating the content frame back if it can go back.
         private void BackButton_Click(object _, RoutedEventArgs _2)
         {
-            if (ContentFrame.BackStackDepth > 0)
+            if (ContentFrame.CanGoBack)
             {
                 ContentFrame.GoBack();
             }
