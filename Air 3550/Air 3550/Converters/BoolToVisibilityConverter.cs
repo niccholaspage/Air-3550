50a8ae82b8d2cb4c03253be91388e503c79e3063
niccholaspage <niccholaspage@gmail.com>
2021-04-27 23:19:42 -0400

    Add comments to BoolToVisibilityConverter

--- Air 3550/Air 3550/Converters/BoolToVisibilityConverter.cs
+++ Air 3550/Air 3550/Converters/BoolToVisibilityConverter.cs
@@ -10,6 +10,13 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * A value converter used to convert a boolean value
+ * to a Visibility for use with XAML bindings. By default,
+ * the visibility if the value is true is visible, otherwise
+ * it is collapsed. This can be configured by the consumer.
+ */
+
 using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Data;
@@ -18,15 +25,25 @@ namespace Air_3550.Converters
 {
     class BoolToVisibilityConverter : IValueConverter
     {
+        // Properties that determine which
+        // visibility is seen based on the
+        // value of the boolean.
         public Visibility OnTrue { get; set; }
         public Visibility OnFalse { get; set; }
 
         public BoolToVisibilityConverter()
         {
+            // By default, a boolean that is
+            // true results in visible, and a
+            // boolean that is false results in
+            // collapsed.
             OnTrue = Visibility.Visible;
             OnFalse = Visibility.Collapsed;
         }
 
+        // This method simply converts the boolean
+        // into the proper visibility, OnTrue or
+        // OnFalse.
         object IValueConverter.Convert(object value, Type targetType, object parameter, string language)
         {
             var boolean = (bool)value;
@@ -34,6 +51,8 @@ namespace Air_3550.Converters
             return boolean ? OnTrue : OnFalse;
         }
 
+        // This method simply converts a visibility
+        // back into a boolean, true or false.
         object IValueConverter.ConvertBack(object value, Type targetType, object parameter, string language)
         {
             if (value is Visibility visibility)
