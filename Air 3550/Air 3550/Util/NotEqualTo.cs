bc7f39d6037623b25a91695a669224566fc67831
niccholaspage <niccholaspage@gmail.com>
2021-04-28 17:22:51 -0400

    Add comments to NotEqualTo

--- Air 3550/Air 3550/Util/NotEqualTo.cs
+++ Air 3550/Air 3550/Util/NotEqualTo.cs
@@ -10,34 +10,47 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * A validation attribute that checks if
+ * the property the attribute is attached to
+ * does not equal another property. If so,
+ * validation is successful. Otherwise, it
+ * returns the ValidationAttribute.ErrorMessage.
+ */
+
 using System.ComponentModel.DataAnnotations;
 
 namespace Air_3550.Util
 {
     class NotEqualTo : ValidationAttribute
     {
-        private readonly string _otherProperty;
+        private readonly string _otherProperty; // Store the name of the other property
 
         public NotEqualTo(string otherProperty)
         {
             _otherProperty = otherProperty;
         }
 
+        // Here we override the ValidationAttribute.IsValid method
+        // to check if our properties equal each other.
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if (value != null)
+            if (value != null) // If the value is not null,
             {
+                // We grab the other property,
                 var otherProperty = validationContext.ObjectInstance.GetType().GetProperty(_otherProperty);
 
+                // then retrieve the value of it.
                 var otherPropertyValue = otherProperty.GetValue(validationContext.ObjectInstance);
 
+                // If the value of our own property equals the other property's value,
                 if (value.Equals(otherPropertyValue))
                 {
-                    return new ValidationResult(ErrorMessage);
+                    return new ValidationResult(ErrorMessage); // return an error.
                 }
             }
 
-            return ValidationResult.Success;
+            return ValidationResult.Success; // Otherwise, we were successful!
         }
     }
 }
