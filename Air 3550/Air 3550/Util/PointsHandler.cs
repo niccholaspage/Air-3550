5d1b2a18f736f9c1b4ef2e9a2b76d7dfd164665c
niccholaspage <niccholaspage@gmail.com>
2021-04-28 17:30:02 -0400

    Add comments to PointHandler

--- Air 3550/Air 3550/Util/PointsHandler.cs
+++ Air 3550/Air 3550/Util/PointsHandler.cs
@@ -10,6 +10,15 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * This static class simply handles
+ * updating a customer's point balance
+ * and retrieving it. It rewards a
+ * customer points when the first departure
+ * or return flight of a booking they have
+ * not canceled takes off.
+ */
+
 using System.Linq;
 using System.Threading.Tasks;
 using Air_3550.Repository;
@@ -19,14 +28,22 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Air_3550.Util
 {
-    public class PointsHandler
+    public static class PointsHandler
     {
+        // We create a record to hold the points balance and total points used of a customer.
         public record PointValues(int RewardPointsBalance, int TotalRewardPointsUsed);
 
+        // This method will retrieve the points balance from the database,
+        // update date it due to bookings that have departed, then return
+        // the updated values to the caller.
         public static async Task<PointValues> UpdateAndRetrievePointsBalance(AirContext db)
         {
+            // We first retrieve the user session service
+            // so we can get the ID of the customer data.
             var userSessionService = App.Current.Services.GetService<UserSessionService>();
 
+            // We query for the customer data, including their bookings,
+            // tickets, scheduled flights, flights, and airports.
             var customerData = await db.CustomerDatas
                 .Include(customerData => customerData.Bookings)
                 .ThenInclude(booking => booking.Tickets)
@@ -40,19 +57,27 @@ namespace Air_3550.Util
                 .ThenInclude(flight => flight.DestinationAirport)
                 .SingleOrDefaultAsync(customerData => customerData.CustomerDataId == userSessionService.CustomerDataId);
 
+            // We set a variable for the new points
+            // we will be rewarding the customer.
             int newPoints = 0;
 
-            foreach (var booking in customerData.Bookings)
+            foreach (var booking in customerData.Bookings) // Now we loop through each booking,
             {
-                var departureTickets = booking.GetDepartureTickets();
-                var returnTickets = booking.GetReturnTickets();
+                var departureTickets = booking.GetDepartureTickets();   // and grab the departure
+                var returnTickets = booking.GetReturnTickets();         // and return tickets.
 
+                /// If none of the departure tickets have been canceled and no points have been earned
+                /// and the first departure flight has departed, we award the user points and flag the
+                /// tickets as having their points earned.
                 if (departureTickets.All(ticket => !ticket.IsCanceled && !ticket.PointsEarned) && booking.DepartureFlightPathWithDate.HasFirstFlightDeparted())
                 {
                     booking.GetDepartureTickets().ForEach(ticket => ticket.PointsEarned = true);
                     newPoints += (int)(booking.DepartureFlightPathWithDate.FlightPath.Price * 10);
                 }
 
+                /// If none of the return tickets have been canceled and no points have been earned
+                /// and the first return flight has departed, we award the user points and flag the
+                /// tickets as having their points earned.
                 if (booking.HasReturnTickets && returnTickets.All(ticket => !ticket.IsCanceled && !ticket.PointsEarned) && booking.ReturnFlightPathWithDate.HasFirstFlightDeparted())
                 {
                     booking.GetReturnTickets().ForEach(ticket => ticket.PointsEarned = true);
@@ -60,6 +85,9 @@ namespace Air_3550.Util
                 }
             }
 
+            // We then update the customer's reward points balance
+            // if our new points are greater than zero, than save
+            // to the database.
             if (newPoints > 0)
             {
                 customerData.RewardPointsBalance += newPoints;
@@ -67,6 +95,9 @@ namespace Air_3550.Util
                 await db.SaveChangesAsync();
             }
 
+            // Finally, we return the point values to the caller so
+            // they can do what they will with them, like display them
+            // in the UI.
             return new PointValues(customerData.RewardPointsBalance, customerData.RewardPointsUsed);
         }
     }
