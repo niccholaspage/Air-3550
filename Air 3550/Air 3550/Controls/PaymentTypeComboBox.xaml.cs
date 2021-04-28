e2c659a91b2107b2524bfffea337cc618fcbd49a
niccholaspage <niccholaspage@gmail.com>
2021-04-27 22:36:21 -0400

    Rename PaymentTypeDropDownButton to PaymentTypeComboBox and add comments

--- Air 3550/Air 3550/Controls/PaymentTypeComboBox.xaml.cs
+++ Air 3550/Air 3550/Controls/PaymentTypeComboBox.xaml.cs
@@ -10,37 +10,48 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+// This control simply wraps a ComboBox
+// and populates it with payment method
+// from the PaymentMethod enumeration.
+
 using System;
 using Database.Models;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Air_3550.Controls
 {
-    public sealed partial class PaymentTypeDropDownButton : UserControl
+    public sealed partial class PaymentTypeComboBox : UserControl
     {
-        public static readonly DependencyProperty SelectedPaymentMethodProperty = DependencyProperty.Register(nameof(SelectedPaymentMethod), typeof(PaymentMethod), typeof(EntitySuggestBox), new PropertyMetadata(default(PaymentMethod)));
-
+        // We define a property consumers can bind to
+        // to get the selected payment method of the
+        // combo box.
         public PaymentMethod SelectedPaymentMethod
         {
             get => (PaymentMethod)GetValue(SelectedPaymentMethodProperty);
             set => SetValue(SelectedPaymentMethodProperty, value);
         }
 
-        public PaymentTypeDropDownButton()
+        public static readonly DependencyProperty SelectedPaymentMethodProperty = DependencyProperty.Register(nameof(SelectedPaymentMethod), typeof(PaymentMethod), typeof(EntitySuggestBox), new PropertyMetadata(default(PaymentMethod)));
+
+
+        public PaymentTypeComboBox()
         {
             this.InitializeComponent();
 
+            // We loop through each payment method and add the
+            // nicely formatted version of it to the combo box.
             foreach (PaymentMethod paymentMethod in Enum.GetValues(typeof(PaymentMethod)))
             {
                 PaymentComboBox.Items.Add(paymentMethod.FormattedString());
             }
 
+            // We set the default selection to the first item.
             PaymentComboBox.SelectedIndex = 0;
 
+            // When the selection changes, we make
+            // sure to update our selected payment
+            // method.
             PaymentComboBox.SelectionChanged += (_, __) =>
             {
                 SelectedPaymentMethod = (PaymentMethod)PaymentComboBox.SelectedIndex;
