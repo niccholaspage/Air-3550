bd94d24dd361e6d56dbb6436b140ff5b4a645f93
niccholaspage <niccholaspage@gmail.com>
2021-04-28 20:21:34 -0400

    Finish RegisterPage comments

--- Air 3550/Air 3550/Views/RegisterPage.xaml.cs
+++ Air 3550/Air 3550/Views/RegisterPage.xaml.cs
@@ -10,6 +10,14 @@
 // Date:		April 28, 2021
 // Copyright:	Copyright 2021 by Nicholas Nassar, Jacob Hammitte, and Nikesh Dhital. All rights reserved.
 
+/**
+ * This page is shown to the user when they
+ * need to register for an account. We do not
+ * handle registration for elevated roles, so
+ * when making an account through the registration
+ * form, a customer will always be created.
+ */
+
 using System;
 using System.Collections.Generic;
 using Air_3550.ViewModels;
@@ -17,29 +25,23 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace Air_3550.Views
 {
     public sealed partial class RegisterPage : Page
     {
-        
-        //On construction a register page is created
-        // for the user to enter informaiton in to 
-        // create a user for the Air 3550 system
         public RegisterPage()
         {
             this.InitializeComponent();
         }
 
-        // Holds the params of the a log in page to bring
-        // The userID over to the log in page
+        // Holds the params of the login page's redirect
+        // to bring it back over to the login page after
+        // an account is created.
         private LoginPage.Params.RedirectToPage redirectParams;
 
-        // Overides OnNavigatedTo to bring redirectParams
-        // if params are RedirectParams then RedirectParams
-        // Will be passed
+        // Overides OnNavigatedTo to set the redirect params
+        // if this page was given them so that we can pass them
+        // back to the login page on successful account creation.
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
@@ -50,12 +52,13 @@ namespace Air_3550.Views
             }
         }
 
-        readonly RegisterViewModel ViewModel = new();
+        readonly RegisterViewModel ViewModel = new();  // Construct the view model.
 
-        // On click of the Register Button
-        // An account will be created with the 
-        // Information provided by the user after
-        // being validated
+        // On a click of the register button,
+        // an account will be created with the 
+        // information provided by the user as
+        // long as it is validated by the view
+        // model.
         private async void RegisterButton_Click(object _, RoutedEventArgs __)
         {
             var loginId = await ViewModel.CreateAccount();
@@ -63,8 +66,15 @@ namespace Air_3550.Views
             //Checks if an account was created
             if (loginId != null)
             {
+                // If so, we construct a new parameter for the login page,
+                // passing in the new user ID so the page will be able to
+                // display it.
                 var newUserParams = new LoginPage.Params.NewUser(loginId);
 
+                // We also make sure to include the redirect parameters
+                // if our register page has them. If so, we pass a list
+                // of parameters to the login page. Otherwise, we just
+                // pass the new user parameters.
                 if (redirectParams == null)
                 {
                     Frame.Navigate(typeof(LoginPage), newUserParams);
